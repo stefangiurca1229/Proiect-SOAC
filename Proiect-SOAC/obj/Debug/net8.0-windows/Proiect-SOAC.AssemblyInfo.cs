d84315b0c5ce833e6c50ac5b9a931c73e7091300
AndyVanP <125876003+AndyVanP@users.noreply.github.com>
2025-01-02 18:16:01 +0200

    implemented last 2 techniques

--- Proiect-SOAC/obj/Debug/net8.0-windows/Proiect-SOAC.AssemblyInfo.cs
+++ Proiect-SOAC/obj/Debug/net8.0-windows/Proiect-SOAC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proiect-SOAC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f874adcfb487b51a056cd1997e0ed6bec7b3f58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proiect-SOAC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proiect-SOAC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
